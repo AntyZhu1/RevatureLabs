786d39d7b7f544a422562235d8ddd670c7db77bb
AntyZhu1 <anthony.zhu@revature.net>
2021-07-13 15:40:52 -0400

    Created and Edited Lab - Edit, Compile, Execute

---
+++ Lab - Edit, Compile, Execute/src/Greeter.java
@@ -0,0 +1,13 @@
+
+public class Greeter {
+
+	public Greeter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+
+	}
+
+}
