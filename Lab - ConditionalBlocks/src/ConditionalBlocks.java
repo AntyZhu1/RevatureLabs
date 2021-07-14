8b9eaec608ef0434d0b227a4d2581a83c62fe3ff
AntyZhu1 <anthony.zhu@revature.net>
2021-07-14 12:27:36 -0400

    added ConditionalBlocks Lab

---
+++ Lab - ConditionalBlocks/src/ConditionalBlocks.java
@@ -0,0 +1,15 @@
+
+public class ConditionalBlocks {
+
+	public static void main(String[] args) {
+		boolean b = true;
+		
+		if (b) {
+			System.out.println("inside the if-statement");
+		}
+		
+		System.out.println("outside the if-statement");
+
+	}
+
+}
