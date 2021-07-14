b5bae09109b82e54aeb0a9cc9567a03df1d6dec7
AntyZhu1 <anthony.zhu@revature.net>
2021-07-14 15:58:09 -0400

    Added Nested Conditionals Lab

--- Lab - ConditionalBlocks/src/ConditionalBlocks.java
+++ Lab - ConditionalBlocks/src/ConditionalBlocks.java
@@ -9,7 +9,26 @@ public class ConditionalBlocks {
 		}
 		
 		System.out.println("outside the if-statement");
+		
+	    boolean firstCondition = false;
+	    boolean secondCondition = true;
 
+	    if (firstCondition){
+	    	System.out.println("inside the if-statement");
+	    	if (secondCondition){
+	    		System.out.println("inside the nested if-statement");
+	    		}
+	    	}
+	    
+	    else if (secondCondition){
+	    	System.out.println(5);
+	    	}
+	    
+	    else {
+	    	System.out.println("inside the else-statement");
+	    	}
+	    
+	    System.out.println("Outside of the if-statement");
 	}
 
 }
