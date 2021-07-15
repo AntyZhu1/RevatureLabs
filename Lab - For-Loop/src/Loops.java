a5ebecaf5351a6025a93353dbbe4477e97e33c7d
AntyZhu1 <anthony.zhu@revature.net>
2021-07-15 14:12:05 -0400

    added several labs

---
+++ Lab - For-Loop/src/Loops.java
@@ -0,0 +1,12 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// Loop to print a number which changes every loop by 1
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
